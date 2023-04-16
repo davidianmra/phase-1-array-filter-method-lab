f5f59d7f15e7467de1c9d5fad5253fa144815404
David Rashid <davidmra@umich.edu>
2023-04-16 17:23:18 -0400

    Finished this lab WOOOH git push

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+
+function findMatching(driverArray, desiredName){
+    
+    const charDriverArray = driverArray
+    const matchingName = driverArray.filter(name => name.toUpperCase() === desiredName.toUpperCase())
+    
+    return matchingName
+    }
+  
+function fuzzyMatch(driverArray, letterCriteria){
+   
+    return driverArray.filter(drivers => drivers.slice(0, letterCriteria.length) === letterCriteria)
+
+    }
+    
+
+function matchName(driverObject, nameCriteria){
+   return driverObject.filter(driver => driver.name === nameCriteria)
+}
+
